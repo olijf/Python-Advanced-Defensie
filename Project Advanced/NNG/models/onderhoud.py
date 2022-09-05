fdb5411f6e1ba14c28facf86957f84ee9877417c
Peter Anema <email@peteranema.nl>
2022-09-05 15:19:12 +0200

    Update

--- Project Advanced/NNG/models/onderhoud.py
+++ Project Advanced/NNG/models/onderhoud.py
@@ -13,17 +13,33 @@ class Onderhoud:
     def __repr__(self):
         return ' - '.join(map(str, self.__dict__.values()))
 
+    def add_onderdeel(self, onderdeel):
+        self.onderdelen.append(onderdeel)
+
+    def add_verbruiksmiddel(self, verbruiksmiddel):
+        self.verbruiksmiddelen.append(verbruiksmiddel)
+
+    def add_tool(self, tool):
+        self.tooling.append(tool)
+
 
 # --------------------------------------------------------------------
 
 if __name__ == '__main__':
+
+    from tool import Tool
 
     onderhoudsbeurten = []
 
     onderhoudsbeurten.append( Onderhoud('Inspectie', '2022-09-05', 'Peter', 'OK') )
     onderhoudsbeurten.append( Onderhoud('Regulier onderhoud', '2022-09-05', 'Peter', 'OK') )
     onderhoudsbeurten.append( Onderhoud('Regulier onderhoud', '2022-08-05', 'Peter', 'OK') )
-    onderhoudsbeurten.append( Onderhoud('Spoed onderhoud', '2022-08-15', 'Peter', 'OK') )
+
+    onderhoud = Onderhoud('Spoed onderhoud', '2022-08-15', 'Peter', 'OK')
+    onderhoud.add_tool( Tool('Steeksleutel 10', '34534534') )
+    onderhoud.add_tool( Tool('Steeksleutel 12', '34534523') )
+    onderhoud.add_tool( Tool('Schoevedraaier Philips', '345345') )
+    onderhoudsbeurten.append( onderhoud )
 
     for onderhoud in onderhoudsbeurten:
         print(onderhoud)
