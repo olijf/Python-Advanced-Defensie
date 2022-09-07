66d72ae65f85869b435f06b9ce45f3571f2e90e9
Peter Anema <email@peteranema.nl>
2022-09-07 10:08:55 +0200

    Update

--- Project Advanced/NNG/forms/materiaal_detail.py
+++ Project Advanced/NNG/forms/materiaal_detail.py
@@ -4,63 +4,57 @@ import tkinter.messagebox
 class App(tk.Frame):
 
     def __init__(self, master=None):
-        self.teller = 0
 
-        w = tk.Label(master, text="Materiaal")
+        w = tk.Label(master, text="Materiaal", font=("Arial", 25))
         w.grid(row = 1, column = 1, columnspan = 2)
 
         w = tk.Label(master, text="Naam")
         w.grid(row = 2, column = 1)
 
-        self.text1 = tk.StringVar()
-        w = tk.Entry(master, textvariable=self.text1)
+        self.naam = tk.StringVar()
+        w = tk.Entry(master, textvariable=self.naam)
         w.grid(row = 2, column = 2)
-
-
 
         w = tk.Label(master, text="Omschrijving")
         w.grid(row = 3, column = 1)
 
-        self.text2 = tk.StringVar()
-        w = tk.Entry(master, textvariable=self.text2)
+        self.omschrijving = tk.StringVar()
+        w = tk.Entry(master, textvariable=self.omschrijving)
         w.grid(row = 3, column = 2)
-
 
         w = tk.Label(master, text="Atacode")
         w.grid(row = 4, column = 1)
 
-        self.text2 = tk.StringVar()
-        w = tk.Entry(master, textvariable=self.text2)
+        self.atacode = tk.StringVar()
+        w = tk.Entry(master, textvariable=self.atacode)
         w.grid(row = 4, column = 2)
-
 
         w = tk.Label(master, text="Type")
         w.grid(row = 5, column = 1)
 
-        self.text2 = tk.StringVar()
-        w = tk.Entry(master, textvariable=self.text2)
+        self.type = tk.StringVar()
+        w = tk.Entry(master, textvariable=self.type)
         w.grid(row = 5, column = 2)
-
 
         w = tk.Label(master, text="Partnummer")
         w.grid(row = 6, column = 1)
 
-        self.text2 = tk.StringVar()
-        w = tk.Entry(master, textvariable=self.text2)
+        self.partnummer = tk.StringVar()
+        w = tk.Entry(master, textvariable=self.partnummer)
         w.grid(row = 6, column = 2)
 
         w = tk.Label(master, text="Serienummer")
         w.grid(row = 7, column = 1)
 
-        self.text2 = tk.StringVar()
-        w = tk.Entry(master, textvariable=self.text2)
+        self.serienummer = tk.StringVar()
+        w = tk.Entry(master, textvariable=self.serienummer)
         w.grid(row = 7, column = 2)
 
         w = tk.Label(master, text="Positie")
         w.grid(row = 8, column = 1)
 
-        self.text2 = tk.StringVar()
-        w = tk.Entry(master, textvariable=self.text2)
+        self.positie = tk.StringVar()
+        w = tk.Entry(master, textvariable=self.positie)
         w.grid(row = 8, column = 2)
 
         w = tk.Button(master, text="Opslaan", command=self.click_handler)
